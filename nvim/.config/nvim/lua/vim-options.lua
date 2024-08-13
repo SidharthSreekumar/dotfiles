9074a7e9500aa5a83c0a899ce00c52791b877a97
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2024-08-13 19:06:09 -0400

    added nvim-colorizer, neofetch, and i3 polybar config changes

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set clipboard=unnamedplus")
 vim.g.mapleader = " "
 vim.g.background = "light"
 
--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Terminate already running bar instances
 killall -q polybar
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini toph &
+    MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini main &
   done
 else
-  polybar --reload toph &
+  polybar --reload main -c ~/.config/polybar/config.ini &
 fi
