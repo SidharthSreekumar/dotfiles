3e21e8b0f1219a6af2a2f6bcf62bae17b39d1545
Sidharth Sreekumar <sidarth.sreekumar@gmail.com>
2024-08-17 17:13:38 -0400

    replaced nvim config following Josean Martinez

---
+++ nvim/.config/nvim/lua/kaizen/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_background = 'soft'
+      vim.cmd.colorscheme('gruvbox-material')
+    end
+  },
+}
